31c908b8f485dc6bee8ea49725b85bb828b3e5b7
olehkuznets <okuzn@softserveinc.com>
2021-09-27 18:42:44 +0300

    SVGICON003: remove redundant code

--- src/app/icon/icon.module.ts
+++ src/app/icon/icon.module.ts
@@ -1,15 +1,11 @@
 import * as IconsCatalog from './icons';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IconComponent } from '../icon/icon.components';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
-    FormsModule,
     ...IconsCatalog.List
   ],
   declarations: [IconComponent],
